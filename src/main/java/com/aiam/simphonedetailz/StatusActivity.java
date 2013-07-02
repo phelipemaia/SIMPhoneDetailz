64b211ae516165e3a7d1bcef12da0b27f8ca5dff
Phelipe Maia <phelipemaia@gmail.com>
2013-07-01 22:47:31 -0300

    Basics files

---
+++ src/main/java/com/aiam/simphonedetailz/StatusActivity.java
@@ -0,0 +1,48 @@
+package com.aiam.simphonedetailz;
+
+import android.os.Bundle;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+@SuppressWarnings("deprecation")
+public class StatusActivity extends TabActivity {
+
+    @SuppressWarnings("deprecation")
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        TabHost host = getTabHost();
+        
+        TabSpec statusspec = host.newTabSpec("Phone");
+        statusspec.setIndicator("Phone",getResources().getDrawable(R.drawable.nphone));
+        Intent phoneStatusIntent = new Intent(this, PhoneStatusActivity.class);
+        statusspec.setContent(phoneStatusIntent);
+        
+        TabSpec batteryspec = host.newTabSpec("Battery");
+        batteryspec.setIndicator("Battery", getResources().getDrawable(R.drawable.nbattery));
+        Intent batteryIntent = new Intent(this, BatteryStatusActivity.class);
+        batteryspec.setContent(batteryIntent);
+
+        /*TabSpec contactsspec = host.newTabSpec("Contacts");
+        batteryspec.setIndicator("Contacts", getResources().getDrawable(R.drawable.nbattery));
+        Intent contactsIntent = new Intent(this, BatteryStatusActivity.class);
+        batteryspec.setContent(contactsIntent);*/
+
+        // Adding all TabSpec to TabHost
+        host.addTab(statusspec); 
+        host.addTab(batteryspec);
+        //host.addTab(contactsspec);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.phonestatus, menu);
+        return true;
+    }
+}
